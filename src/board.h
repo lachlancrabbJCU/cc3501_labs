e460276fe03b13c472c379bd8f709744d0f84a59
lachlancrabbJCU <140355342+lachlancrabbJCU@users.noreply.github.com>
2024-09-28 11:44:47 +1000

    Add pin layout header file

---
+++ src/board.h
@@ -0,0 +1,19 @@
+#pragma once
+
+// GPIO pins
+#define SW1 15
+#define ACCEL_SDA 16
+#define ACCEL_SCL 17
+#define ACCEL_SCLK 2
+#define ACCEL_MOSI 3
+#define ACCEL_MISO 4
+#define ACCEL_CS 5
+#define ACCEL_INT 6
+#define ACCEL_INT2 7
+#define LED_PIN 14
+#define NUMBER_OF_LEDS 12
+#define MIC_OUT 26
+
+// Accelerometer
+#define ACCEL_I2C_INSTANCE i2c0
+#define ACCEL_I2C_ADDRRESS 0x19
